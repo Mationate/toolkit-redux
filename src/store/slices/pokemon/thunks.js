e7a37aa04fe7e312c1a226603327a366df3aaab3
Matias Onate <mationate.0@gmail.com>
2023-01-04 18:29:09 -0300

    axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,12 +1,14 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemon, startLoadingPokemon } from "./PokemonSlice"
 
 export const getPokemon = (page =0) => {
     return async(dispatch, getState) => {
         dispatch(startLoadingPokemon());
         // TODO hacer peticion http 
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page*10}`);
-        const data = await resp.json();
-        console.log(data);
-        // dispatch(setPokemon());
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page*10}`);
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page*10}`);
+        // const data = await resp.json();
+        // console.log(data);
+        dispatch(setPokemon({pokemons: data.results, page: page +1}));
     }
 }
